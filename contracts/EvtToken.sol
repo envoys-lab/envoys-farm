13dc41954fa28493d076cd71aedefbf60e47a2c8
Sergey <63466627+xsubject@users.noreply.github.com>
2022-08-29 18:45:34 +0300

    Update EvtToken.sol

--- contracts/EvtToken.sol
+++ contracts/EvtToken.sol
@@ -3,7 +3,7 @@ pragma solidity 0.6.12;
 import "@pancakeswap/pancake-swap-lib/contracts/token/BEP20/BEP20.sol";
 
 // EvtToken with Governance.
-contract EvtToken is BEP20('EnvoysSwap Token', 'Evt') {
+contract EvtToken is BEP20('EnvoysSwap Token', 'EVT') {
     /// @notice Creates `_amount` token to `_to`. Must only be called by the owner (MasterChef).
     function mint(address _to, uint256 _amount) public onlyOwner {
         _mint(_to, _amount);
