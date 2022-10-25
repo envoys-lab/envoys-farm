dd35d459d57662377459433e3e480799b1c223f8
Sergey <dev.zaycev@gmail.com>
2022-10-25 17:58:33 +0300

    update deps

--- contracts/EvtToken.sol
+++ contracts/EvtToken.sol
@@ -1,6 +1,6 @@
 pragma solidity 0.6.12;
 
-import "@pancakeswap/pancake-swap-lib/contracts/token/BEP20/BEP20.sol";
+import "@envoysvision/envoys-swap-lib/contracts/token/BEP20/BEP20.sol";
 
 // EvtToken with Governance.
 contract EvtToken is BEP20('EnvoysSwap Token', 'EVT') {
